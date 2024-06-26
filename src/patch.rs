a43877139d57df85d577aad885da0e76efec217e
spore <spore2050@gmail.com>
2024-06-27 01:23:15 +0800

    refactor!: stop storing the whole patch in memory

--- src/patch.rs
+++ src/patch.rs
@@ -3,50 +3,15 @@ use std::{
     io::{self, BufRead, Seek, Write},
 };
 
-use eyre::{eyre, OptionExt, WrapErr};
+use eyre::{eyre, WrapErr};
 
 use crate::{
     cli::PatchArgs,
     parse::{for_parsed_data, recognize_line, DumpLine},
 };
 
-struct Patch {
-    offset: u64,
-    data: Vec<u8>,
-}
-
-type PatchSet = Vec<Patch>;
-
-fn parse_patch<R: BufRead>(reader: R) -> eyre::Result<PatchSet> {
-    let mut patch_set: PatchSet = Vec::new();
-    for line in reader.lines() {
-        let line = line?;
-        let DumpLine { offset, data, .. } = recognize_line(&line);
-        let mut parsed_data = Vec::new();
-        for_parsed_data(data, |b| {
-            parsed_data.push(b);
-            Ok(())
-        })?;
-        if offset.is_empty() {
-            patch_set
-                .last_mut()
-                .ok_or_eyre("patch must start with a valid offset")?
-                .data
-                .extend(parsed_data);
-        } else {
-            let offset = u64::from_str_radix(offset, 16)
-                .wrap_err_with(|| eyre!("invalid offset `{offset}`"))?;
-            patch_set.push(Patch {
-                offset,
-                data: parsed_data,
-            });
-        }
-    }
-    Ok(patch_set)
-}
-
 pub fn patch(args: PatchArgs) -> eyre::Result<()> {
-    let mut target = File::options()
+    let target = File::options()
         .write(true)
         .read(false)
         .open(&args.target)
@@ -59,12 +24,23 @@ pub fn patch(args: PatchArgs) -> eyre::Result<()> {
 
     let input = io::stdin().lock();
 
-    let patch_set = parse_patch(input)?;
+    patch_impl(input, target)
+}
 
-    for Patch { offset, data } in patch_set {
-        target.seek(io::SeekFrom::Start(offset))?;
-        target.write_all(&data)?;
+pub(crate) fn patch_impl<R: BufRead, W: Write + Seek>(reader: R, mut writer: W) -> eyre::Result<()> {
+    for line in reader.lines() {
+        let line = line?;
+        let DumpLine { offset, data, .. } = recognize_line(&line);
+        if !offset.is_empty() {
+            let offset = u64::from_str_radix(offset, 16)
+                .wrap_err_with(|| eyre!("invalid offset `{offset}`"))?;
+            writer.seek(io::SeekFrom::Start(offset))?;
+        }
+        for_parsed_data(data, |b| {
+            writer
+                .write_all(&[b])
+                .wrap_err("failed to write to target file")
+        })?;
     }
-
     Ok(())
 }
